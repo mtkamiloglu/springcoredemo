76e44bd0b517d3abe70922f5ed46799ad834c16a
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-04-24 00:22:27 +0300

    organize folders and setter injection

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,5 +1,6 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
 
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach){
+    public void setCoach(Coach theCoach){
         this.myCoach = theCoach;
     }
 
@@ -18,4 +19,5 @@ public class DemoController {
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
+
 }
