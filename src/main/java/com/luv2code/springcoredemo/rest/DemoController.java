99d781a20e44350df9021a10a3d6927ca820adcd
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-04-24 10:52:11 +0300

    new classes are added and qualifier is used

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,16 +2,17 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    private Coach myCoach;
+    private final Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){ // we used qualifier because there are more than one Coach implementation
         this.myCoach = theCoach;
     }
 
