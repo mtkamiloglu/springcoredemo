0c2cde8eb3cb7717e035f8202a58cc7ba8e4bbb4
M. Tayyip Kamiloglu <mt.kamiloglu@gmail.com>
2023-04-24 13:49:20 +0300

    added scopes

--- src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcoredemoApplication {
+public class  SpringcoredemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcoredemoApplication.class, args);
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -10,15 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private final Coach myCoach;
+    private final Coach anotherCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){ // we used qualifier because there are more than one Coach implementation
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach theAnotherCoach)
+    {
+        System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = theCoach;
+        this.anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach is " + (myCoach == anotherCoach);
     }
 
 }
